e4941e14d133c774a2aa77628ed6b12fec148718
Roman Zots <vinegreet@gmail.com>
2020-07-23 21:42:02 +0300

    Update build

---
+++ build/precache-manifest.4cd37e25dd673c77e2caaf6465c0edd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5620cce27c01d203c325716ab68b6f30",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3838d9e8983c16610fc8",
+    "url": "./static/css/main.101d41f5.chunk.css"
+  },
+  {
+    "revision": "1c81724eff231ebc535e",
+    "url": "./static/js/2.e57fddd8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.e57fddd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3838d9e8983c16610fc8",
+    "url": "./static/js/main.fee73f3e.chunk.js"
+  },
+  {
+    "revision": "d0224b77f5617baa36e8",
+    "url": "./static/js/runtime-main.6d09b29e.js"
+  }
+]);
